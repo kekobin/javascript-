2ec011d074af9a95ebbac91726902fe053af95bb
System Administrator <root@myWIFI.local>
2016-08-24 07:51:05 +0800

    add rem

---
+++ rem/gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var sass = require("gulp-sass");
+
+gulp.task('sass',function(){
+	return gulp.src("./sass/*.scss")
+		.pipe(sass())
+		.pipe(gulp.dest("./css/"));
+})
+
+
+gulp.task('default',function(){
+	// gulp.start('sass');
+
+	return gulp.watch("./sass/*.scss", ['sass']);
+})
